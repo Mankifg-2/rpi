73842d8e14b5c9487ca2af69cb1630fa725f78e9
Mankifg-2 <jaka.horvat@ospuconci.si>
2022-10-11 13:47:48 +0200

    revert

--- led.py
+++ led.py
@@ -1,27 +1,34 @@
+#!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
 
-
 LedPin = 17
 
+def setup():
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LedPin, GPIO.OUT, initial=GPIO.HIGH)
 
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LedPin, GPIO.OUT, initial=GPIO.HIGH)
-
-
-while True:
-    try:
-        print ('[+]', end=", ")
-
+def main():
+    while True:
+        print ('...LED ON')
+        # Turn on LED
         GPIO.output(LedPin, GPIO.LOW)
-        
         time.sleep(0.5)
-        print ('[-]', end=", ")
+        print ('LED OFF...')
+        # Turn off LED
         GPIO.output(LedPin, GPIO.HIGH)
+        time.sleep(0.5)
 
-        time.sleep(0.5)
-        
+
+def destroy():
+    GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.cleanup()                   
+
+
+if __name__ == '__main__':
+    setup()
+    try:
+        main()
     except KeyboardInterrupt:
-        GPIO.output(LedPin, GPIO.HIGH)
-        GPIO.cleanup()                   
+        destroy()
